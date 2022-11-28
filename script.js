f2aba377ac6dc1c633f15f07abf5dc086503ad64
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2022-11-28 19:16:22 +0100

    hitup-project

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// asynchronous way
+// this line of code is executed after the bg taks is finished
+setTimeout(()=>console.log('test'), 5000)
+
+console.log('no');
+console.log('tak');
+
